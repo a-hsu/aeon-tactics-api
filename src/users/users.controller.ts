c5e6ec7717a5fb0a7b34d42a3987d07d91f96ae7
a-hsu <a.hsu873@gmail.com>
2023-04-13 20:12:29 -0400

    test user model in prisma and controller for basic sign up with no security. testing for deployment

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+
+import { User as UserModel } from '@prisma/client';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Post('new')
+  async signupUser(
+    @Body() userData: { name?: string; email: string },
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+}
